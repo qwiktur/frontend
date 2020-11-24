a46f20cf3b8bf7e5e6e70b03990140e78210b311
Miki Namsrai <miki.namsrai@gmail.com>
2020-11-24 11:18:40 +0100

    Add tailwind to project

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const App: React.FC = () => (
-    <></>
-)
+const App: React.FC = () => {
+    return (
+      <div className="App">
+        <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal m-10" type="email" placeholder="jane@example.com" />
+      </div>
+    )
+  }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './tailwind.output.css'
 import App from './components/app';
 import reportWebVitals from './reportWebVitals';
 
