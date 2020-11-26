31d38bf857270d104e5e2371ac676858e43ea317
Rocknologie <rocknologie@hotmail.fr>
2020-11-26 12:23:47 +0100

    New color for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                green: {
+                    light: '#00a000',
+                    DEFAULT: '#009000',
+                    dark: '#006600',
+                }
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
