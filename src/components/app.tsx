5d03f677379b6f27dbb8b89cb60ab50a9ef31a08
Rocknologie <rocknologie@hotmail.fr>
2020-12-11 16:59:17 +0100

    Routing

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AuthenticationProvider from './context-providers/authentication-provider';
+import React from 'react';
+import { BackOfficeUserContainer } from './back-office/page-user-backoffice/back-office-user-container';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { DataManagementContainer } from './back-office/page-data-management/data-management-container';
 import { HomePage } from './home-page/home-page';
 import { Navbar } from './navbar/navbar';
 
@@ -13,9 +15,13 @@ const App: React.FC = () => {
             <Navbar />
             <HomePage />
           </Route>
-          <Route exact path="/backoffice">
+          <Route exact path="/backoffice/data-management">
             <Navbar />
-            <HomePage />
+            <DataManagementContainer />
+          </Route>
+          <Route exact path="/backoffice/users">
+            <Navbar />
+            <BackOfficeUserContainer />
           </Route>
         </Switch>
       </BrowserRouter>
