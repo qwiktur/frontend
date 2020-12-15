ddaa7e95591bbe95bb0fa374cf39e71eb14ccff1
Miki Namsrai <miki.namsrai@gmail.com>
2020-12-15 15:36:20 +0100

    create lobby hidden if user is disconnected

--- src/components/context-providers/authentication-provider.tsx
+++ src/components/context-providers/authentication-provider.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
-import AuthenticationContext from "../../contexts/authentication-context";
-import useFetch from "../../hooks/fetch-hook";
-import { Config } from "../../util/config";
-import { LocalStorageKey } from "../../util/local-storage";
-import { UserData } from "../../util/types/data-types";
-import { UserInfoResponse } from "../../util/types/response-types";
+import React, { useEffect, useState } from 'react';
+import AuthenticationContext from '../../contexts/authentication-context';
+import useFetch from '../../hooks/fetch-hook';
+import { Config } from '../../util/config';
+import { LocalStorageKey } from '../../util/local-storage';
+import { UserData } from '../../util/types/data-types';
+import { UserInfoResponse } from '../../util/types/response-types';
 
 const AuthenticationProvider: React.FC = (props) => {
     const [authUser, setAuthUser] = useState<UserData>(null);
@@ -22,7 +22,7 @@ const AuthenticationProvider: React.FC = (props) => {
             }
         }
     }, [userInfoQueryState.fetched]);
-  
+
     return (
         <AuthenticationContext.Provider value={{ authUser, isAuthenticated: authUser != null, setAuthUser }}>
             {props.children}
